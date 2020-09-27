00655d7c7768b5e679b8d90fdc49e7831bba37b1
Usman Adebanjo <oluwatobilobalayeuthman@gmail.com>
2020-09-27 17:43:51 +0100

    Controllers
    Controllers uploaded both API controllers and

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
+using System.Data.Entity;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Get/Api/Customers
+        public IEnumerable<CustomerDto> GetCustomers()
+        {
+            return _context.Customers
+                .Include(c=>c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer,CustomerDto>);
+        }
+
+        //Get/Api/Customer/1
+
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers
+                .Include(c=>c.MembershipType)
+                .SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+               return NotFound();
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        //Post/Api/Customer
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                customerDto.Id = customer.Id; 
+                 return Created(new Uri(Request.RequestUri + "/"+ customer.Id),customerDto);
+            } 
+        }
+        //PUT/Api/customerDto/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                
+                var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+                if (customerInDb == null)
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                Mapper.Map(customerDto, customerInDb);
+            }
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        //Delete/Api/customerDto/id
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                _context.Customers.Remove(customerInDb);
+            }
+            
+             _context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.ModelBinding;
+using Vidly.Models;
+using Vidly.ViewModel;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New(Customer customer )
+        {   
+            var membershipType = _context.MembershipTypes.ToList();
+
+            var viewModel = new NewCustomerViewModel
+            {
+                Customer=customer,
+                MembershipTypes = membershipType
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModelCustomer = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm",viewModelCustomer);
+            }
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+                customerInDb.BirthDate = customer.BirthDate;
+              
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                //customerInDb.Name = customer.Name;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customers");
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+            
+        }
+    }
+}
