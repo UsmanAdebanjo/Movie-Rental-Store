935aafa52c6097ce2b999173c060abc1b0a625d8
Usman Adebanjo <oluwatobilobalayeuthman@gmail.com>
2020-09-27 17:51:48 +0100

    DTO's
    Data transfer object for Customer, Movie and Membership Types

---
+++ Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+       // public Genre Genre { get; set; }
+
+
+        public byte GenreId { get; set; }
+
+        [Required]
+
+        [Range(1, 30)]
+        public int AmountInStock { get; set; }
+
+        [Required]
+        public DateTime PurchasedDate { get; set; }
+        [Required]
+        public DateTime ReleasedDate { get; set; }
+    }
+}
